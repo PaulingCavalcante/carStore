67bd71b0d853e72120c042950b9c55dd9936bef6
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2024-09-12 18:28:56 -0300

    Update info-car.js

--- js/info-car.js
+++ js/info-car.js
@@ -1,19 +1,19 @@
 const carData = {
     1: {
-        brand: 'Toyota',
+        brand: 'Renault Kwid E-Tech',
         year: '2020',
         price: '75000',
         img: 'https://cdn.motor1.com/images/mgl/vxoxbG/s4/renault-kwid-e-tech---avaliacao---externas-2.webp'
     },
     2: {
-        brand: 'Honda',
+        brand: 'BYD Dolphin Mini',
         year: '2018',
         price: '68000',
         img: 'https://cdn.motor1.com/images/mgl/Oo0v7l/s4/byd-dolphin-mini-br.webp'
     },
     3: {
-        brand: 'Ford',
-        year: '2021',
+        brand: 'Caoa Cherry iCar',
+        year: '2023',
         price: '80000',
         img: 'https://cdn.motor1.com/images/mgl/XBBrO9/s4/caoa-chery-icar-2023-1.webp'
     }
